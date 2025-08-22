0f1bb7cd10d0b1bc6ac42edf1f146e379ce12358
Robert Waterman <rwaterman0451@floridapoly.edu>
2025-08-22 15:46:52 +0000

    First program

---
+++ 01_intro_to_coding/first_program.c
@@ -0,0 +1,20 @@
+/
+
+Multiline comment.
+08/20/2025
+Robert Waterman
+The first program in C.
+
+*/
+
+// One line comment
+
+// Preprocessor directive
+// stdio.h - standard input output
+// .h - header file
+#include <stdio.h>
+
+// create the main function
+int main(void) {
+    printf("Hello World");
+}
